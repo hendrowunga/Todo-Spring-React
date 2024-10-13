1ea87cd7f4579ff9db838c3522248a8324d29fc6
Hendro <hendrowunga073@gmail.com>
2024-10-14 01:12:45 +0700

    controllers

--- Server/src/main/java/com/hendro/controllers/TutorialController.java
+++ Server/src/main/java/com/hendro/controllers/TutorialController.java
@@ -21,6 +21,110 @@ import java.util.Optional;
 @Tag(name = "Todo")
 @RequestMapping("/api")
 public class TutorialController {
+    @Autowired
+    TutorialRepository tutorialRepository;
+
+    @Operation(summary = "Get all Todo", description = "Retrieve a list of all todo")
+    @GetMapping("/todo")
+    public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
+        try {
+            List<Tutorial> tutorials = new ArrayList<>();
+
+            if (title == null)
+                tutorialRepository.findAll().forEach(tutorials::add);
+            else
+                tutorialRepository.findByTitleContaining(title).forEach(tutorials::add);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Get todo by ID", description = "Retrieve a todo by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the tutorial"),
+            @ApiResponse(responseCode = "404", description = "Tutorial not found")
+    })
+    @GetMapping("/todo/{id}")
+    public ResponseEntity<Tutorial> getTutorialById(@Parameter(description = "ID of the todo to be retrieved") @PathVariable("id") long id) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+        if (tutorialData.isPresent()) {
+            return new ResponseEntity<>(tutorialData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation(summary = "Create a todo", description = "Add a new todo")
+    @PostMapping("/todo")
+    public ResponseEntity<Tutorial> createTutorial(@RequestBody Tutorial tutorial) {
+        try {
+            Tutorial _tutorial = tutorialRepository
+                    .save(new Tutorial(tutorial.getTitle(), tutorial.getDescription(), false));
+            return new ResponseEntity<>(_tutorial, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Update a todo", description = "Update an existing todo by ID")
+    @PutMapping("/todo/{id}")
+    public ResponseEntity<Tutorial> updateTutorial(@Parameter(description = "ID of the todo to be updated") @PathVariable("id") long id, @RequestBody Tutorial tutorial) {
+        Optional<Tutorial> tutorialData = tutorialRepository.findById(id);
+
+        if (tutorialData.isPresent()) {
+            Tutorial _tutorial = tutorialData.get();
+            _tutorial.setTitle(tutorial.getTitle());
+            _tutorial.setDescription(tutorial.getDescription());
+            _tutorial.setPublished(tutorial.isPublished());
+            return new ResponseEntity<>(tutorialRepository.save(_tutorial), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation(summary = "Delete a todo", description = "Delete a todo by ID")
+    @DeleteMapping("/todo/{id}")
+    public ResponseEntity<HttpStatus> deleteTutorial(@Parameter(description = "ID of the todo to be deleted") @PathVariable("id") long id) {
+        try {
+            tutorialRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Delete all todo", description = "Delete all todo")
+    @DeleteMapping("/todo")
+    public ResponseEntity<HttpStatus> deleteAllTutorials() {
+        try {
+            tutorialRepository.deleteAll();
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Operation(summary = "Get published todo", description = "Retrieve a list of published todo")
+    @GetMapping("/todo/published")
+    public ResponseEntity<List<Tutorial>> findByPublished() {
+        try {
+            List<Tutorial> tutorials = tutorialRepository.findByPublished(true);
+
+            if (tutorials.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(tutorials, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 
 }
